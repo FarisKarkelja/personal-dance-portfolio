2902c5bcd142c7cb3a3842f8fbfb01cdabb5d785
Faris Karkelja <faree5803@gmail.com>
2024-03-31 12:35:57 +0200

    project1 final code

---
+++ project1/script.js
@@ -0,0 +1,16 @@
+/* Get the modal */
+
+var modal = document.getElementById('myModal');
+
+/* When the page finishes loading, display the modal */
+
+window.onload = function() {
+  modal.style.display = 'block';
+}
+
+/* Close the modal */
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
