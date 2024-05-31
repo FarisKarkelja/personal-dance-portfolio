12ed65b02d6ee4ec81da136183a0db0d271640d3
Faris Karkelja <faree5803@gmail.com>
2024-05-31 15:26:26 +0200

    project2

--- project2/js/editAnddelete.js
+++ project2/js/editAnddelete.js
@@ -1,39 +1,3 @@
-/*const questionForm = document.getElementById('questionForm');
-const userQuestion = document.getElementById('userQuestion');
-const successButton = document.getElementById('success');
-
-successButton.addEventListener('click', function() {
-  const question = userQuestion.value.trim();
-  
-  if (question !== '') {
-    showQuestion(question);
-    userQuestion.value = '';
-  }
-});
-
-function showQuestion(question) {
-  const newQuestion = document.createElement('div');
-  newQuestion.textContent = question;
-  
-  const editButton = document.createElement('button');
-  editButton.textContent = 'Edit';
-  editButton.addEventListener('click', function() {
-    userQuestion.value = question;
-    questionForm.removeChild(newQuestion);
-  });
-  
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Delete';
-  deleteButton.addEventListener('click', function() {
-    questionForm.removeChild(newQuestion);
-  });
-  
-  newQuestion.appendChild(editButton);
-  newQuestion.appendChild(deleteButton);
-  
-  questionForm.appendChild(newQuestion);
-}*/
-
 const questionForm = document.getElementById('questionForm');
 const userQuestion = document.getElementById('userQuestion');
 const successButton = document.getElementById('success');
